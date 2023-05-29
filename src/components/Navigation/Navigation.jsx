5378da515258dbc449aeba44f95c69334c848364
Shane-757 <underwoodshane@ymail.com>
2023-05-28 21:02:44 -0400

    update

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import UserMenu from '../UserMenu/UserMenu';
-import { Box } from '@chakra-ui/react';
+import { Box} from '@chakra-ui/react';
 import NavLink from '../NavLink/NavLink'; 
 
-  const Navigation = () => {
+const Navigation = () => {
   const location = useLocation();
   const isAuthenticated = !!localStorage.getItem('userToken');
+
+  useEffect(() => {
+  console.log('Page changed to', location.pathname);
+}, [location]);
   
   return (
     <Box as="nav">
       <NavLink to="/register" fontSize="lg">
         Register
       </NavLink>
-      <Box width="10px" mt={5} />
+      <Box width="10px" mt={4} />
       <NavLink to="/login" fontSize="lg">
         Login
       </NavLink>
-      <Box width="10px" my={5} />
+      <Box width="10px" my={4} />
       <NavLink to="/contacts" fontSize="lg">
         Contacts
       </NavLink>
